09074625155f87148ca7b597c21eda96cd97a16f
András Bencze <bencze.andras0402@gmail.com>
2024-10-08 12:26:15 +0200

    Added cmake files - for test purposes

---
+++ Dependencies.cmake
@@ -0,0 +1,9 @@
+# Dependencies
+
+set(GLFW_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/GLFW/include" CACHE STRING "GLFW include directory")
+set(GLM_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/glm" CACHE STRING "GLM include directory")
+set(SPDLOG_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/spdlog/include" CACHE STRING "spdlog include directory")
+set(GLAD_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/glad/include" CACHE STRING "Glad include directory")
+set(IMGUI_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/ImGui" CACHE STRING "ImGui include directory")
+set(ENTT_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/entt/include" CACHE STRING "entt include directory")
+set(TINYGLTF_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/Aurora/Vendor/tinygltf/include" CACHE STRING "tinygltf include directory")
